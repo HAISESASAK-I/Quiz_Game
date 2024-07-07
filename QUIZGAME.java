82ce2bfb1df643693c69af03a7c915e202dccb18
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2024-07-07 18:09:19 +0500

    Add the QUIZGAME.java file

---
+++ QUIZGAME.java
@@ -0,0 +1,7 @@
+public class QUIZGAME {
+    public static void main (String [] args){
+        System.out.println("<-------------------------------------Quiz Game--------------------------------------------------");
+        
+    }
+     
+}
