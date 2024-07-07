bd5b5501039b635ae920e90a4b9d5f6b8621d6c0
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2024-07-07 18:21:39 +0500

    Add the quiz menu for user

--- QUIZGAME.java
+++ QUIZGAME.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class QUIZGAME {
-    public static void main (String [] args){
-        System.out.println("<-------------------------------------Quiz Game--------------------------------------------------");
-        
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(
+            "<-------------------------------------Quiz Game--------------------------------------------------");
+        System.out.println(
+            "What do you like to do:\nEnter \"1\" to start the Quiz.\nEnter \"2\" to add questions in Quiz game.\nEnter \"3\" to view your current score.");
+        int userChoice = scanner.nextInt();
+        switch (userChoice) {
+        case 1: {
+
+            break;
+        }
+        case 2: {
+
+            break;
+        }
+        case 3: {
+            break;
+        }
+
+        default: {
+            System.out.println(
+                "You have entered an invalid input\n Kindly enter a valid input");
+        }
+        }
     }
-     
 }
